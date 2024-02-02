bfbfca8ddb22df935233c3144a81044b59d2b83d
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-02-02 22:22:20 +0700

    edit web.php but undone the change at the end

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/login_influencer', function () {
         return view('login_influencer');
