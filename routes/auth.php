f5a7c7151f1e56c96c2255137c6bc506592219aa
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-02-15 09:29:48 +0700

    add route for influencer facebook login

--- routes/auth.php
+++ routes/auth.php
@@ -9,11 +9,16 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\SocialAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
+    
+    Route::get('auth/{provider}', [RegisteredUserController::class, 'redirectToProvider']);
+
+    Route::get('auth/{provider}/callback', [RegisteredUserController::class, 'handleProviderCallback']);
         
 
     Route::post('register', [RegisteredUserController::class, 'store']);
