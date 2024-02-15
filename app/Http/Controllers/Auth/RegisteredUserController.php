ddd9b68e3077150eeac5adcf0eb4f47eb04eb3c0
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-02-15 09:03:39 +0700

    add controller for social media login

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Laravel\Socialite\Facades\Socialite;
+
 
 class RegisteredUserController extends Controller
 {
@@ -65,5 +67,35 @@ class RegisteredUserController extends Controller
       
 
         return redirect(RouteServiceProvider::HOME);
+    }
+
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function handleProviderCallback($provider)
+    {
+        $user = Socialite::driver($provider)->stateless()->user();
+        $authUser = $this->findOrCreateUser($user, $provider);
+        Auth::login($authUser, true);
+        return redirect()->route('dashboard');
+    }
+
+    public function findOrCreateUser($user, $provider)
+    {   
+       
+        $authUser = User::where('provider_id', $user->id)->first();
+        if ($authUser) {
+            return $authUser;
+        }
+
+        return User::create([
+            'name' => $user->name,
+            'email' => !empty($user->email)? $user->email : '' ,
+            'provider' => $provider,
+            'provider_id' => $user->id,
+            'role' => 'influencer',
+        ]);
     }
 }
