7f01f23c60306517f8ba983ad8631b823f16825a
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-02-24 09:56:00 +0700

    add migration for create influencers table

---
+++ database/migrations/2024_02_22_162233_create_influencers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('influencers', function (Blueprint $table) {
+            $table->id();
+            $table->date('birth_date');
+            $table->string('gender');
+            $table->text('about_me');
+            $table->string('profile_picture_path')->nullable();
+            $table->string('address');
+            $table->boolean('is_married')->default(false);
+            $table->unsignedBigInteger('province_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('province_id')->references('id')->on('provinces')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('influencers');
+    }
+};
