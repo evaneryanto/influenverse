86050f2a6ce4eb347a62b783eb090f02d1743605
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-03-03 12:05:14 +0700

    add function province and categories for relation to provinces and category_influencer tables

--- app/Models/Influencer.php
+++ app/Models/Influencer.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Influencer extends Model
 {
@@ -19,5 +21,16 @@ class Influencer extends Model
         'user_id',
         'phone_number'
     ];
+
+
+    public function province() : BelongsTo
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function categories() : BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
 
 }
