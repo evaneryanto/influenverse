fb803cc449f689447b792afb5d286983dae3bcb0
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-03-10 14:08:47 +0700

    add gate authorization for check influencer, check brand, and influencer authorization

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Providers;
-
-// use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use App\Models\Portfolio;
+use App\Models\Influencer;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Ramsey\Uuid\Type\Integer;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,17 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('check_influencer', function (User $user) {
+            return $user->role == 'influencer' || $user->role == 'admin';
+        });
+
+        Gate::define('influencer_authorization', function (User $user, Influencer $influencer) {
+            return $user->id == $influencer->user_id;
+        });
+
+        Gate::define('check_brand', function (User $user) {
+            return $user->role == 'brand' || $user->role == 'admin';
+        });
+
     }
 }
